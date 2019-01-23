5e1e71955dfe7ad9bffd0aac2700111c9bfc3732
Christopher McFarland <christopherdmcfarland@gmail.com>
2019-01-23 11:48:05 -0800

    documentation

--- distributed_cluster/SLURM_job_submission.sh
+++ distributed_cluster/SLURM_job_submission.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+# This is an example submission script of parameter_sweep_job.py that will run on Stanford's Sherlock2 Cluster.
+# The cluster uses the SLURM job scheduler. 
+
+
 NJOBS=2
 DIRECTORY=`pwd`
 NAME=`basename $DIRECTORY`
@@ -7,7 +11,7 @@ MEMORY="128GB" # Normal node contains 128 GB of memory & 20 cores
 #MEMORY="196GB" # Normal node contains 128 GB of memory & 20 cores
 NODES=1
 CORES=20 #24
-SCRIPT="$HOME/pdSim/single_sherlock_job.py"
+SCRIPT="$HOME/pdSim/parameter_sweep_job.py"
 QUEUE="owners,hns"
 TIME=2-0
 
